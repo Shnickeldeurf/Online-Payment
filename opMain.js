fbe9b02e9fbf9e3ac3b366c9cb0c62b3def81fe3
Shnickeldeurf <sbeckertech@gmail.com>
2023-10-10 12:59:57 -0400

    working buttons

--- opMain.js
+++ opMain.js
@@ -1,35 +1,52 @@
 $(document).ready(function(){
-
+    side = $('header .text-end');
+    if (current != null) {
+        side.innerHTML = 'You are logged in as: ' + current.name;
+    }
 })
 
+//get set users
+if (localStorage.getItem('opUsers') == null) {
+    users = [];
+    localStorage.setItem('opUsers', JSON.stringify(users));
+} else {
+    users = JSON.parse(localStorage.getItem('opUsers'));
+}
+
+if (localStorage.getItem('opCurrentUser') !== null) {
+    current = JSON.parse(localStorage.getItem('opCurrentUser'));
+}
+
+//show modal
 function showModal() {
     modal = $('#myModal');
 
     modal.modal('show');
 }
 
-function change() {
-    buttons = $('.model .btn-light');
-    forms = $('.model form');
+//show login-register
+function showReg() {
+    b1 = $('#logTab');
+    b2 = $('#regTab');
+    reg = $('#register');
+    log = $('#login');
 
-    console.log(buttons);
-    console.log(forms);
+    b1.removeClass('active');
+    b2.addClass('active');
 
-    for (let i = 0; i < buttons.length; i++) {
-        if ($(buttons[i]).hasClass('active')) {
-            buttons[i].classList.remove('active');
-            buttons[i].onclick = change;
-        } else {
-            buttons[i].classList.add('active');
-            buttons[i].onclick = null;
-        }
-    }
+    reg.show();
+    log.hide();
+}
 
-    for (let i = 0; i < forms.length; i++) {
-        if ($(forms[i]).hasClass('inActive')) {
-            forms[i].classList.remove('inActive');
-        } else {
-            forms[i].classList.add('inActive');
-        }
-    }
+function showLogin() {
+    b1 = $('#logTab');
+    b2 = $('#regTab');
+    reg = $('#register');
+    log = $('#login');
+
+    b1.addClass('active');
+    b2.removeClass('active');
+
+    reg.hide();
+    log.show();
 }
